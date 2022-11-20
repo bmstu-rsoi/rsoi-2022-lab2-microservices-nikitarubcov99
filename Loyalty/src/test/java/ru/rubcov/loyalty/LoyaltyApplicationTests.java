23d66ae0697d409852435a9d3a8309605f4a1063
nikitarubcov99 <nikitarubstov99@gmail.com>
2022-11-20 22:44:49 +0300

    fix: fixed loyalty application test

---
+++ Loyalty/src/test/java/ru/rubcov/loyalty/LoyaltyApplicationTests.java
@@ -0,0 +1,15 @@
+package ru.rubcov.loyalty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = LoyaltyApplicationTests.class)
+public class LoyaltyApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
+
+
+
+
